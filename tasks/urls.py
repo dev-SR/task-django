dc9f967bb08a37aa3a4f3ea0c3956ed69d3a23aa
Soikat Rahman <hello.dev.sr@gmail.com>
2024-02-01 15:02:54 +0600

    task crud api implemented

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path, include
 from . import views
+from .api.views import TaskViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
 urlpatterns = [
     path('', views.TaskList.as_view(), name='task-list'),
     path('task/create/', views.CreateTaskView.as_view(), name='task-create'),
@@ -9,4 +13,5 @@ urlpatterns = [
     path('task/<pk>/update/', views.UpdateTaskView.as_view(), name='task-update'),
     path('task/<pk>/delete/', views.DeleteTaskView.as_view(), name='task-delete'),
     path('delete_photo/<int:photo_id>/<int:task_id>/', views.delete_photo, name='delete-photo'),
+    path('api/', include(router.urls)),
 ]
